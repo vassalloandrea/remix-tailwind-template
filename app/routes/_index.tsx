55fdfb9f7996c02d787408cae87b078e8c4d91d2
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2024-04-15 09:23:59 +0200

    Add static PLP to the homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
-import type { MetaFunction } from '@remix-run/node'
+// Route file naming docs: https://remix.run/docs/en/main/file-conventions/routes
+
+import { type MetaFunction } from '@remix-run/node'
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,12 +9,31 @@ export const meta: MetaFunction = () => {
   ]
 }
 
+const PRODUCT_HANDLES = ['developer-cap', 'developer-tshirt', 'hoodie']
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+    <div
+      style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}
+      className="container mx-auto my-5 flex flex-col gap-y-5"
+    >
       <h1 className="text-3xl font-bold underline">
         Welcome to Remix, Nebulab!
       </h1>
+
+      <div>
+        <h2 className="mb-2 text-lg font-semibold text-gray-900">
+          List of products:
+        </h2>
+
+        <ul className="max-w-md space-y-1 text-gray-500 list-disc list-inside">
+          {PRODUCT_HANDLES.map((handle) => (
+            <li key={handle}>
+              <a href={`/products/${handle}`}>{handle}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
